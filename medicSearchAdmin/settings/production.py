157013e2be60a260852d347fc48df0cab47f1b4c
GustavoBruxo <gustavo-henrique92@hotmail.com>
2024-02-17 21:45:28 -0300

    Livro Django de A a Z Capítulo 1 a 3

---
+++ medicSearchAdmin/settings/production.py
@@ -0,0 +1,19 @@
+from .settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-q2m5ip395^o45hdiaciu8ccw9-61fy7g+_d88#n7^ks855s=8&'
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}

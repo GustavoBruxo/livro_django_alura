053b54f4ef0784ed4b64a901348599225dc496d7
GustavoBruxo <gustavo-henrique92@hotmail.com>
2024-02-22 17:33:11 -0300

    Capítulo 7 Trabalhando com Templates - Parte 1

--- medicSearch/models/Profile.py
+++ medicSearch/models/Profile.py
@@ -1,6 +1,5 @@
-# -*- Coding: UTF-8 -*-
 from medicSearch.models import *
-
+from django.db.models import Sum, Count
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,8 +11,8 @@ class Profile(models.Model):
     image = models.ImageField(null=True, blank=True)
     favorites = models.ManyToManyField(User, blank=True, related_name='favorites')
     specialties = models.ManyToManyField(Speciality, blank=True, related_name='specialties')
-    addresses = models.ManyToManyField(Address, blank=True, auto_created='addresses')
-
+    addresses = models.ManyToManyField(Address, blank=True, related_name='addresses')
+    
     def __str__(self):
         return '{}'.format(self.user.username)
 
@@ -31,3 +30,23 @@ class Profile(models.Model):
             instance.profile.save()
         except:
             pass
+
+    def show_scoring_average(self):
+        from .Rating import Rating
+        try:
+            ratings = Rating.objects.filter(user_rated=self.user).aggregate(Sum('value'), Count('user'))
+            if ratings['user__count'] > 0:
+                scoring_average = ratings['value__sum'] / ratings['user__count']
+                scoring_average = round(scoring_average, 2) # Arredondando o valor para duas casas decimais
+                return scoring_average
+            return 'Sem avaliações'
+        except:
+            return 'Sem avaliações'
+
+    def show_favorites(self):
+        ids = [result.id for result in self.favorites.all()]
+        return Profile.objects.filter(user__id__in=ids)
+
+    def show_ratings(self):
+        from .Rating import Rating
+        return Rating.objects.filter(user_rated=self.user)
